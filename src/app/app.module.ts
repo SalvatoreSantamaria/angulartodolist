1a4ea221521e5b8b6af74eb3caf7160fa5adf99a
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-06-01 09:17:27 -0700

    Fixed Component Error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
-import { HeaderComponent } from './component/layout/header/header.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
   declarations: [
